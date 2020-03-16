57725b0f7d83f57a72f840187a9e719a0853e450
llwyd <tom@llwyd.io>
2020-03-16 22:03:29 +0000

    file writing to verify frequency response and tidied up some of the formatting

--- realtime/pink.c
+++ realtime/pink.c
@@ -15,21 +15,12 @@
 
 typedef struct
 {
-	float b[3]; /* Feed forward*/
-	float a[3]; /* Feed backward*/
-}sos_t;
-
-typedef struct
-{
 	float y;
 	float *s[SOS_STAGES];
 	float *b[SOS_STAGES];
 	float *a[SOS_STAGES];
+	FILE *fp;
 }pa_callbackdata;
-
-
-
-
 
 static int Pink_FilterCallback( const void *input,
                                       void *output,
@@ -68,24 +59,26 @@ static int Pink_FilterCallback( const void *input,
                                 unsigned long frameCount,
                                 const PaStreamCallbackTimeInfo* timeInfo,
                                 PaStreamCallbackFlags statusFlags,
-								void *callbackData)
+				void *callbackData)
 {
 	pa_callbackdata * data = (pa_callbackdata*)callbackData;
 	float * out = (float *)output;
 
 	float y = data->y;	
 	float x;
-	
+
 	for(int i=0;i<frameCount;i++)
 	{
 		x = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
+
 		for(int j=0;j<SOS_STAGES;j++)
 		{
-			y				=(data->b[j][0]*x)						+ data->s[j][0];
+			y		=(data->b[j][0]*x)			+ data->s[j][0];
 			data->s[j][0]	=(data->b[j][1]*x) - (data->a[j][1]*y) 	+ data->s[j][1];
 			data->s[j][1]	=(data->b[j][2]*x) - (data->a[j][2]*y);
 			x = y;
 		}
+		fprintf(data->fp,"%g\n",y);
 		*out++ = y;
 	}
 	data->y=y;
@@ -97,7 +90,7 @@ int main( int argc, char ** argv)
 {
 	PaStream * audioStream;
 	pa_callbackdata data;
-	
+
 	float b1[3]={ 0.23529412, -0.0178079 ,  0.f };
 	float a1[3]={ 1.        , -0.53567505,  0.f};
 
@@ -118,36 +111,19 @@ int main( int argc, char ** argv)
 
 	data.y=0.0f;
 
+	data.fp = fopen("pink.txt","w");
+
 	srand((unsigned int)time(NULL));
 	Pa_Initialize();
 
 	Pa_OpenDefaultStream(&audioStream,0,1,paFloat32,FS,128,Pink_FilterCallback,&data);
 	Pa_StartStream(audioStream);
-		
 	printf("Generating pink noise\n");
 
 	Pa_Sleep(5000);
 	Pa_StopStream(&audioStream);
 	Pa_CloseStream(&audioStream);
 	Pa_Terminate();
+	fclose(data.fp);
 	return 0;
 }
-
-	/* Generate White noise */
-	
-	/*
-	FILE * fp = fopen("pink.txt","w");
-	for(int i=0;i<samples;i++)
-	{
-		
-		y0[i]=		(b0[0]*white[i])+s0[0];
-		s0[0]=s0[1]+	(b0[1]*white[i])-(a0[1]*y0[i]);
-		s0[1]=		(b0[2]*white[i])-(a0[2]*y0[i]);
-
-		y1[i]=		(b1[0]*y0[i])+s1[0];
-		s1[0]=s1[1]+	(b1[1]*y0[i])-(a1[1]*y1[i]);
-		s1[1]=		(b1[2]*y0[i])-(a1[2]*y1[i]);
-		fprintf(fp,"%g\n",y1[i]);
-	}
-	fclose(fp);
-	*/	
