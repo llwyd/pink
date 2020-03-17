40bc912b1e7418856c057af372cfe56074bcf34a
llwyd <tom@llwyd.io>
2020-03-17 21:03:27 +0000

    pink noise filteirng, but in stereo! this code is a horrific mess, needs tidying

--- realtime/pink.c
+++ realtime/pink.c
@@ -12,6 +12,7 @@
 
 #define FS ( 44100 ) /* Hz */
 #define SOS_STAGES ( 2 )
+#define CHANNELS ( 2 )
 
 typedef struct
 {
@@ -21,6 +22,16 @@ typedef struct
 	float *a[SOS_STAGES];
 	FILE *fp;
 }pa_callbackdata;
+
+typedef struct
+{
+	float yl;
+	float yr;
+	float *sr[SOS_STAGES];
+	float *sl[SOS_STAGES];
+	float *b[SOS_STAGES];
+	float *a[SOS_STAGES];
+}pa_stereocallbackdata;
 
 static int Pink_FilterCallback( const void *input,
                                       void *output,
@@ -42,7 +53,7 @@ static int Pink_WhiteCallback( 	const void *input,
                                 unsigned long frameCount,
                                 const PaStreamCallbackTimeInfo* timeInfo,
                                 PaStreamCallbackFlags statusFlags,
-								void *callbackData)
+				void *callbackData)
 {
 	float * out = (float *)output;
 		
@@ -54,12 +65,7 @@ static int Pink_WhiteCallback( 	const void *input,
 	return 0;
 }
 
-static int Pink_FilterCallback( const void *input,
-                                void *output,
-                                unsigned long frameCount,
-                                const PaStreamCallbackTimeInfo* timeInfo,
-                                PaStreamCallbackFlags statusFlags,
-				void *callbackData)
+static int Pink_FilterCallback( const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *callbackData)
 {
 	pa_callbackdata * data = (pa_callbackdata*)callbackData;
 	float * out = (float *)output;
@@ -78,7 +84,6 @@ static int Pink_FilterCallback( const void *input,
 			data->s[j][1]	=(data->b[j][2]*x) - (data->a[j][2]*y);
 			x = y;
 		}
-		fprintf(data->fp,"%g\n",y);
 		*out++ = y;
 	}
 	data->y=y;
@@ -86,10 +91,48 @@ static int Pink_FilterCallback( const void *input,
 	return 0;
 }
 
+
+static int Pink_StereoFilterCallback( const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *callbackData)
+{
+	pa_stereocallbackdata * data = (pa_stereocallbackdata*)callbackData;
+	float * out = (float *)output;
+
+	float yr = data->yr;	
+	float yl = data->yl;
+	float xl;
+	float xr;
+
+	for(int i=0;i<(frameCount);i++)
+	{
+		xl = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
+		xr = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
+
+		for(int j=0;j<SOS_STAGES;j++)
+		{
+			yl		=(data->b[j][0]*xl)				+ data->sl[j][0];
+			yr		=(data->b[j][0]*xr)				+ data->sr[j][0];
+			data->sl[j][0]	=(data->b[j][1]*xl) - (data->a[j][1]*yl) 	+ data->sl[j][1];
+			data->sr[j][0]	=(data->b[j][1]*xr) - (data->a[j][1]*yr) 	+ data->sr[j][1];
+			data->sl[j][1]	=(data->b[j][2]*xl) - (data->a[j][2]*yl);
+			data->sr[j][1]	=(data->b[j][2]*xr) - (data->a[j][2]*yr);
+			xl = yl;
+			xr = yr;
+		}
+		*out++ = yl;
+		*out++ = yr;
+	}
+	data->yl=yl;
+	data->yr=yr;
+
+	return 0;
+}
+
+
 int main( int argc, char ** argv)
 {
 	PaStream * audioStream;
 	pa_callbackdata data;
+	pa_stereocallbackdata stereoData;
 
 	float b1[3]={ 0.23529412, -0.0178079 ,  0.f };
 	float a1[3]={ 1.        , -0.53567505,  0.f};
@@ -99,6 +142,11 @@ int main( int argc, char ** argv)
 
 	float s0[2]={0.0f};
 	float s1[2]={0.0f};
+
+	float sl0[2]={0.0f};
+	float sl1[2]={0.0f};
+	float sr0[2]={0.0f};
+	float sr1[2]={0.0f};
 
 	data.b[0]=b0;
 	data.b[1]=b1;
@@ -111,12 +159,23 @@ int main( int argc, char ** argv)
 
 	data.y=0.0f;
 
-	data.fp = fopen("pink.txt","w");
+	stereoData.yr=0.0f;
+	stereoData.yl=0.0f;
+	stereoData.b[0]=b0;
+	stereoData.b[1]=b1;
+	stereoData.a[0]=a0;
+	stereoData.a[1]=a1;
+	stereoData.sl[0]=sl0;
+	stereoData.sl[1]=sl1;
+	stereoData.sr[0]=sr0;
+	stereoData.sr[1]=sr1;
 
 	srand((unsigned int)time(NULL));
 	Pa_Initialize();
 
-	Pa_OpenDefaultStream(&audioStream,0,1,paFloat32,FS,128,Pink_FilterCallback,&data);
+	//Pa_OpenDefaultStream(&audioStream,0,1,paFloat32,FS,128,Pink_FilterCallback,&data);
+	
+	Pa_OpenDefaultStream(&audioStream,0,2,paFloat32,FS,128,Pink_StereoFilterCallback,&stereoData);
 	Pa_StartStream(audioStream);
 	printf("Generating pink noise\n");
 
@@ -124,6 +183,5 @@ int main( int argc, char ** argv)
 	Pa_StopStream(&audioStream);
 	Pa_CloseStream(&audioStream);
 	Pa_Terminate();
-	fclose(data.fp);
 	return 0;
 }
