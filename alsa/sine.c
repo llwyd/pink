2d1834ce1264ea9c09d48ba84027bf98e1734a7b
llwyd <tom@llwyd.io>
2020-10-20 23:00:07 +0100

    helps if I get the endian-ness right, made a sinewave program to test code

--- alsa/sine.c
+++ alsa/sine.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <math.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -22,24 +23,25 @@ int main( int argc, char **argv )
 	snd_pcm_set_params( 	handle,
 				SND_PCM_FORMAT_FLOAT_LE, 	/* little endian*/
 				SND_PCM_ACCESS_RW_INTERLEAVED,	/* interleaved */
-				2,				/* channels */
+				1,				/* channels */
 				44100,				/* sample rate */
 				2,				/* alsa resampling */
 				250000);			/* desired latency */
 	
 	/* run for 4 seconds */
-	float x[2] ={0.0f};
+	float x = 0.0f;
+	float f = 1000.f;
+	float T = 1.f / 44100.f;
+
 	for(int i=0; i < (44100*4); i++)
 	{
-		x[0] = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
-		x[1] = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
-		err = snd_pcm_writei( handle, x, 2);
+		x = sin(2 * M_PI * f * (float)i * T);
+		err = snd_pcm_writei( handle, &x, 1);
 		if( err == -EPIPE )
 		{
 			fprintf(stderr, "Underrun\n");
 		}
 	}
-	
 	snd_pcm_drain( handle );
 	snd_pcm_close( handle );
 	return 0;
