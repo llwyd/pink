612fda01124b92c239bdc49bd805d7fc5606fa5a
llwyd <tom@llwyd.io>
2020-10-20 23:26:51 +0100

    basic pink filtering for alsa

--- alsa/pink.c
+++ alsa/pink.c
@@ -11,6 +11,23 @@
 #include <time.h>
 #include <unistd.h>
 
+
+#define SOS_STAGES 	( 2 )
+#define CHANNELS 	( 2 )
+#define FS 		( 48000 )
+
+const static float b[][3] = 
+{
+	{ 1.f         ,-1.81835938f,  0.82094419f},
+	{ 0.23529412f, -0.0178079f ,  0.f },
+};
+
+const static float a[][3] =
+{
+	{ 1.         ,-1.94363403,  0.9438566},
+	{ 1.        , -0.53567505,  0.f},
+};
+
 int main( int argc, char **argv )
 {
 	snd_pcm_t *handle;
@@ -23,17 +40,40 @@ int main( int argc, char **argv )
 				SND_PCM_FORMAT_FLOAT_LE, 	/* little endian*/
 				SND_PCM_ACCESS_RW_INTERLEAVED,	/* interleaved */
 				2,				/* channels */
-				44100,				/* sample rate */
+				FS,				/* sample rate */
 				2,				/* alsa resampling */
 				250000);			/* desired latency */
 	
 	/* run for 4 seconds */
-	float x[2] ={0.0f};
-	for(int i=0; i < (44100*4); i++)
+	float xl,xr,yl,yr = 0.0f;
+
+	float sl[2][2] = {0.0f};
+	float sr[2][2] = {0.0f};
+
+	for(int i=0; i < (FS*4); i++)
 	{
-		x[0] = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
-		x[1] = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
-		err = snd_pcm_writei( handle, x, 2);
+		xl = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
+		xr = (((float)rand()/(float)RAND_MAX)*2.f)-1.f;
+		
+		for(int j=0; j < SOS_STAGES; j++)
+		{
+			/* left */
+			yl 	 = b[j][0]*xl 		 	+ sl[j][0];
+			sl[j][0] = b[j][1]*xl	- a[j][1]*yl 	+ sl[j][1];
+			sl[j][1] = b[j][2]*xl	- a[j][2]*yl;
+
+			/* right */
+			yr 	 = b[j][0]*xr 		 	+ sr[j][0];
+			sr[j][0] = b[j][1]*xr	- a[j][1]*yr 	+ sr[j][1];
+			sr[j][1] = b[j][2]*xr	- a[j][2]*yr;
+			
+			xl = yl;
+			xr = yr;
+		}
+		
+
+		err = snd_pcm_writei( handle, &yl, 1);
+		err = snd_pcm_writei( handle, &yr, 1);
 		if( err == -EPIPE )
 		{
 			fprintf(stderr, "Underrun\n");
