4b12ad37bb1f31e00535d3062ae29a174c8c7337
llwyd <tom@llwyd.io>
2020-11-22 17:34:51 +0000

    beginnings of an interactive EQ model

---
+++ models/interactive_eq.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.3)
+
+l, = plt.plot([1,2,3,4])
+
+
+axcolor = 'lightgoldenrodyellow'
+axfreq_0 = plt.axes([0.2, 0.225, 0.65, 0.03], facecolor=axcolor)
+axfreq_1 = plt.axes([0.2, 0.175, 0.65, 0.03], facecolor=axcolor)
+axfreq_2 = plt.axes([0.2, 0.125, 0.65, 0.03], facecolor=axcolor)
+axfreq_3 = plt.axes([0.2, 0.075, 0.65, 0.03], facecolor=axcolor)
+axfreq_4 = plt.axes([0.2, 0.025, 0.65, 0.03], facecolor=axcolor)
+
+band_0 = Slider(axfreq_0, 'Band 0', 0.01, 100, valinit=1, valstep=0.5)
+band_1 = Slider(axfreq_1, 'Band 1', 0.01, 100, valinit=1, valstep=0.5)
+band_2 = Slider(axfreq_2, 'Band 2', 0.01, 100, valinit=1, valstep=0.5)
+band_3 = Slider(axfreq_3, 'Band 3', 0.01, 100, valinit=1, valstep=0.5)
+band_4 = Slider(axfreq_4, 'Band 4', 0.01, 100, valinit=1, valstep=0.5)
+
+
+plt.show()
+
