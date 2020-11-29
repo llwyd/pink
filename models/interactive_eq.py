be523c723a6ae6d728727b3191990d1baefab634
llwyd <tom@llwyd.io>
2020-11-29 19:51:48 +0000

    progress in developing interactive EQ model

--- models/interactive_eq.py
+++ models/interactive_eq.py
@@ -1,13 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
+from scipy import signal
+
+class EQBand():
+    def __init__(self,lp_cutoff,hp_cutoff,fs):
+        order = 1
+        self.fs = fs
+        self.gain = 1.0
+        if hp_cutoff == 0:
+            # low pass only
+            self.high = [1,0,0,1,0,0]
+        else:
+            self.high = signal.butter(1,hp_cutoff,'hp',fs=fs,output='sos')
+
+        if lp_cutoff == 0:
+            # high pass only
+            self.low = [1,0,0,1,0,0]
+        else:
+            self.low = signal.butter(1,lp_cutoff,'lp',fs=fs,output='sos')
+            
+def fft(x,fs,fft_len):
+    F = np.fft.fft(x,fft_len,norm='ortho')
+    F = np.abs(F)
+    F = norm(F)
+    Ff = (fs/2)*np.linspace(0,1,int(fft_len/2))
+    Fdb = 20*np.log10(F[:int(len(F)/2)]);
+    
+    return F, Ff, Fdb
+
+def norm( n ):
+    return n/np.max(np.abs(n))
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.35)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude (dB)')
 
-l, = plt.plot([1,2,3,4])
+fs = 44100
+band = EQBand(500,1000,fs)
+sig_len = 8192 * 8 
+h = signal.unit_impulse(sig_len)
+x = signal.sosfilt(band.low,h)
+x = signal.sosfilt(band.high,x)
+
+X,Xf,Xdb = fft(x,fs,sig_len)
+
+l, = plt.semilogx(Xf,Xdb)
+plt.hlines(-3,0,max(Xf))
+plt.ylim(-30,5)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Magnitude (dB)')
 
 def update_graph(val):
     print(band_0.val)
