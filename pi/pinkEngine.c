0262fd3b39e87ee15e72bd6ce74b8534e79ca52d
llwyd <tom@llwyd.io>
2020-12-12 23:01:14 +0000

    command line arguments working

--- pi/pinkEngine.c
+++ pi/pinkEngine.c
@@ -1,4 +1,9 @@
-
+/*
+*
+*       pinkEngine.c
+*       T.L. 2020
+*
+*/
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -11,23 +16,75 @@
 #include <unistd.h>
 #include <time.h>
 
+typedef struct
+{
+    int h;
+    int m;
+    int s;
+} time_setup_t;
+
 int main( int argc, char ** argv )
 {
+    /* variables relating to input parameters */
+    int inputFlags;
+    int inputCount = 0;
+    time_setup_t timeSetup;
+    memset( &timeSetup, 0x00, sizeof(timeSetup));    
+
+    /* runtime time variables */
     time_t startTime;
     time_t currentTime;
+    
+    double runTime      = 0;
+    double timeDelta    = 0;
 
-    double runLength = 10;
-    double timeDelta = 0;
+    printf("--------------------------------\n");
+    printf("\tpinkEngine\n");
+    printf("\t T.L 2020\n");
+    printf("--------------------------------\n");
 
-    time( &startTime );
-    time( &currentTime );
-
-    while( timeDelta < runLength )
-    {
-        timeDelta = difftime( currentTime, startTime );
-        time( &currentTime );
+    /* configure inputs */
+    while((inputFlags = getopt( argc, argv, "h:m:s:")) != -1)
+    {   
+        switch( inputFlags )
+        {
+            case 'h':
+                timeSetup.h = atoi( optarg );
+                inputCount++;
+                break;
+            case 'm':
+                timeSetup.m = atoi( optarg );
+                inputCount++;
+                break;
+            case 's':
+                timeSetup.s = atoi( optarg );
+                inputCount++;
+                break;
+        }
     }
 
-}
+    runTime = (double)( ( timeSetup.h * 3600 ) + ( timeSetup.m * 60 ) + timeSetup.s );
+    
+    if( runTime > 0.0f )
+    {
+        printf( "Generating Pink Noise for %d hour(s), %d minute(s) and %d second(s)\n", timeSetup.h, timeSetup.m, timeSetup.s );
+        printf( "(%.0f second(s))\n", runTime );        
+        
+        time( &startTime );
+        time( &currentTime );
 
+        while( timeDelta < runTime )
+        {
+
+            /* run last */
+            timeDelta = difftime( currentTime, startTime );
+            time( &currentTime );
+        }
+    }
+    else
+    {
+        printf("Missing time information, returning\n");
+    }
+    return 0;
+}
 
